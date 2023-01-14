3baf6014de6dc36f283242bb8beddcbbe959fdcc
Marc raphael <marcraphael979@gmail.com>
2023-01-14 18:19:01 +0100

    App: Fix lnters errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Calculator from './components/calculator';
+import Calculator from './components/calculator.js';
 
 class App extends React.Component {
   render() {
