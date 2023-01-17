2d551415b37a062de01e562f88a42de527da08d5
Marc raphael <marcraphael979@gmail.com>
2023-01-17 23:21:25 +0100

    components/Pages/Quotes: Create quotes component

---
+++ src/components/Pages/Quotes.jsx
@@ -0,0 +1,10 @@
+const Quote = () => (
+  <div className="Quote">
+    <h2>
+      Mathematics is not about numbers, equations, computations,
+      or algorithms: It is about understanding - William Paul Thurston
+    </h2>
+  </div>
+);
+
+export default Quote;
