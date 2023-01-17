c441329e70916b5947028a215893bc2259f0845c
Marc raphael <marcraphael979@gmail.com>
2023-01-17 22:58:11 +0100

    components/Pages/Home: create home component

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => (
+  <div className="Home">
+    <h1>Welcome to the Math magic app</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    </p>
+  </div>
+);
+
+export default Home;
